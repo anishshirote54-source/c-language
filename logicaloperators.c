283b8752d32f7c87d989b9f2602c1d0f99d1ca2b
anishshirote54-source <anishshirote54@gmail.com>
2026-05-07 20:24:40 +0530

    Add files via upload

---
+++ logicaloperators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+    int a, b;
+    a = 30;
+    b = 10;
+    printf("x = %d\n", a&&b);
+    printf("y = %d\n", a||b);
+    printf("z = %d\n", !(a&&b));
+    
+    return 0;
+    
+}
---
+++ relationaloperators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+    int a, b;
+    a = 30;
+    b = 10;
+    printf("x = %d\n", a==b);
+    printf("y = %d\n", a>b);
+    printf("z = %d\n", a<b);
+    printf("s = %d\n", a!=b);
+    
+    return 0;
+    
+}
