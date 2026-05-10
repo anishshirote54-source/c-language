841bf8483ab5f8c37357ffefc330747af5c0b014
anishshirote54-source <anishshirote54@gmail.com>
2026-05-10 21:15:21 +0530

    Add files via upload

---
+++ dowhile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+    int a, i;
+
+    printf("Enter a number -");
+    scanf("%d", &a);
+
+    do
+    {
+        printf("%d", i);
+        i = i + 1;
+    } while (i<10);
+    
+}
